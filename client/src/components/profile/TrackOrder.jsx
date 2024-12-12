294b9ccd324617da542775bc45202ca79e2c0016
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-12-12 19:23:15 +0100

    yuytfg

---
+++ client/src/components/profile/TrackOrder.jsx
@@ -0,0 +1,7 @@
+const TrackOrder = () => {
+  return (
+    <div>TrackOrder</div>
+  )
+}
+
+export default TrackOrder
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Header from "@/components/layout/Header"
+// import ProfileContent from "@/components/profile/ProfileContent"
+import ProfileSidebar from "@/components/profile/ProfileSidebar"
+import { useState } from "react"
 
 const ProfilePage = () => {
+  const [active, setActive] = useState(1)
+  
   return (
-    <div>ProfilePage</div>
+    <div>
+      <Header/>
+      <div className="flex py-10 mx-auto max-pad-container sm:px-6 lg:px-8">
+        <div className="w-[335px] mr-8">
+          <ProfileSidebar active={active} setActive={setActive}/>
+        </div>
+        <div className="flex-1">
+          {/* <ProfileContent active={active} /> */}
+        </div>
+      </div>
+    </div>
   )
 }
 
