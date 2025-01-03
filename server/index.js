7a1ff72cd9f78771b9168730f71abe976162ed90
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-01-03 05:38:25 +0100

    server

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://vendor-l0ew.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
